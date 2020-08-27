dab6bc127a532da8ad970e98bc0aeef2454b2168
Jahid1987 <66158562+Jahid1987@users.noreply.github.com>
2020-08-27 21:54:06 +0600

    Revert "First commit"
    This reverts commit a3b6b47d6107ac2dccbaea165d0fdc8ee01fd96f.

--- resources/views/admin/products/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('admin.app')
-@section('breadcrumb')
-<li class="breadcrumb-item" aria-current="page">Products</li>
-<li class="breadcrumb-item active" aria-current="page">Index</li>
-@endsection
-@section('content')
-<h2>Products</h2>
-@endsection
